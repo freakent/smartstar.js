bdd4d36961fa26b32c2e00c95c6e365c19b6dc80
Martin Jarvis <martin@freakent.co.uk>
2012-11-25 20:13:44 +0000

    Tested version ???

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var topic = 'smartstar/in';
 
 var client = mqtt.createClient(port, host, function(err, client) {
   if (err) process.exit(1);
-  client.connect({keepalive: 60*30});
+  client.connect({keepalive: 15*60});
 
   client.on('connack', function(packet) {
     if (packet.returnCode === 0) {
-		processTwitterStream(client);
+      console.log('Connected to broker');
     } else {
       console.log('connack error %d', packet.returnCode);
       process.exit(-1);
@@ -24,44 +24,45 @@ var client = mqtt.createClient(port, host, function(err, client) {
   });
 
   client.on('close', function() {
-    console.log('Connection to broker closed');
+    console.log('Connection to broker closed: ' + client);
     //process.exit(0);
-    client.connect({keepalive: 60*30});
-    console.log('Reconnected to broker');
   });
 
   client.on('error', function(e) {
     console.log('error %s', e);
     process.exit(-1);
   });
+
+  setInterval(function(){client.publish({topic: "ping", payload: "ping"})}, 10 * 60 * 1000);
   
 });
 
-function processTwitterStream(client) {
-	twitter.stream('statuses/filter', {track:'cheerlights blue,cheerlights green,cheerlights purple'}, function(stream) {
-	
-		stream.on('data', function (data) {
-			var pattern = data.text.toUpperCase().each(/blue|green|purple/i, function(color) {return color.first()}).join(""); 
-			if (pattern.length > 0) {
-			  console.log(pattern);
-			  client.publish({topic: topic, payload: "CHEER " + pattern});
-			}
-		});
-		
-		stream.on('end', function (response) {
-			// Handle a disconnection
-			client.disconnect();
-			console.log("Stream ended");
-		});
-		
-		stream.on('destroy', function (response) {
-			// Handle a 'silent' disconnection from Twitter, no end/error event fired
-			client.disconnect();
-			console.log("Stream destroyed");
-		});
-		
+twitter.stream('statuses/filter', {track:'cheerlights blue,cheerlights green,cheerlights purple, cheerlights'}, function(stream) {
+
+	stream.on('data', function (data) {
+		var pattern = data.text.toUpperCase().each(/\b(red|blue|green|purple)\b/i, function(color) {return color.first()}).join(""); 
+		if (pattern.length > 0) {
+			d = new Date();
+			console.log(d.getHours() + ":" + d.getMinutes() + " - " + pattern + " - " + data.text);
+			client.publish({topic: topic, payload: "CHEER " + pattern});
+		}
 	});
-};
+	
+//  setInterval(function(){console.log(stream)}, 30000);
+
+	stream.on('end', function (response) {
+		// Handle a disconnection
+		console.log("Stream ended :" + response);
+	});
+	
+	stream.on('destroy', function (response) {
+		// Handle a 'silent' disconnection from Twitter, no end/error event fired
+		console.log("Stream destroyed :" + response);
+	});
+	
+});
+
+//setInterval(function(){console.log(twitter)}, 30000);
 
 new cronJob('00 30 19 * * *', function(){
     client.publish({topic: topic, payload: "ON"});
@@ -72,3 +73,4 @@ new cronJob('00 00 22 * * *', function(){
     client.publish({topic: topic, payload: "OFF"});
     console.log('Switch lights off');
 }, null, true);
+
