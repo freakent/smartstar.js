8289b52ae5cf3aba1b5d92284cd2537663f1a196
Martin Jarvis <martin@freakent.co.uk>
2012-11-28 14:03:53 +0000

    Restructured into callback functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var util = require('util');
 var twitterAPI = require('ntwitter');
 var mqtt = require('mqttjs');
 var sugar = require('sugar');
@@ -10,76 +11,89 @@ var host = '192.168.0.12';
 var port = '1883';
 var topic = 'smartstar/in';
 
-var client = mqtt.createClient(port, host, function(err, client) {
-  if (err) process.exit(1);
-  client.connect({keepalive: 15*60});
+function when_connected_to_mqtt(client) {
+  var date = new Date();
+  if (date.getHours() >= 17 && date.getHours() < 23) {
+      client.publish({topic: topic, payload: "ON"});
+      util.log('Switch lights on');
+  } else {
+      client.publish({topic: topic, payload: "OFF"});
+      util.log('Switch lights off');
+  }
 
-  client.on('connack', function(packet) {
-    if (packet.returnCode === 0) {
-      console.log('Connected to broker');
-    } else {
-      console.log('connack error %d', packet.returnCode);
+  new cronJob('00 00 17 * * *', function(){
+  	  client.publish({topic: topic, payload: "ON"});
+    	util.log('Switch lights on');
+	}, null, true);
+
+	new cronJob('00 30 22 * * *', function(){
+  	  client.publish({topic: topic, payload: "OFF"});
+    	util.log('Switch lights off');
+	}, null, true);
+	
+	open_twitter_stream(client);
+};
+
+function connect_to_mqtt(connected_callback) {
+  return mqtt.createClient(port, host, function(err, client) {
+
+    if (err) process.exit(1);
+
+    client.connect({keepalive: 15*60});
+
+    client.on('connack', function(packet) {
+      if (packet.returnCode === 0) {
+        util.log('Connected to broker');
+        connected_callback(client);
+      } else {
+        util.log('connack error %d', packet.returnCode);
+        process.exit(-1);
+      }
+    });
+
+    client.on('close', function() {
+      util.log('Connection to broker closed: ' + client);
       process.exit(-1);
-    }
-  });
+    });
 
-  client.on('close', function() {
-    console.log('Connection to broker closed: ' + client);
-    //process.exit(0);
-  });
+    client.on('error', function(e) {
+      util.log('error %s', e);
+      process.exit(-1);
+    });
 
-  client.on('error', function(e) {
-    console.log('error %s', e);
-    process.exit(-1);
-  });
-
-  setInterval(function(){client.publish({topic: "ping", payload: "ping"})}, 10 * 60 * 1000);
-
-var date = new Date();
-if (date.getHours() >= 17 && date.getHours() < 23) {
-    client.publish({topic: topic, payload: "ON"});
-    console.log('Switch lights on');
-} else {
-    client.publish({topic: topic, payload: "OFF"});
-    console.log('Switch lights off');
-}
+  setInterval( function() { client.publish({topic: "ping", payload: "ping"})}, 10 * 60 * 1000);
   
-});
+  });
+};
 
-twitter.stream('statuses/filter', {track:'cheerlights blue,cheerlights green,cheerlights purple, cheerlights'}, function(stream) {
+function open_twitter_stream(mqtt_client) {
+  util.log("Opening twitter stream");
+	twitter.stream('statuses/filter', {track:'cheerlights blue,cheerlights green,cheerlights purple, cheerlights'}, function(stream) {
 
-	stream.on('data', function (data) {
-		var pattern = data.text.toUpperCase().each(/\b(red|blue|green|purple)\b/i, function(color) {return color.first()}).join(""); 
-		if (pattern.length > 0) {
-			d = new Date();
-			console.log(d.getHours() + ":" + d.getMinutes() + " - " + pattern + " - " + data.text);
-			client.publish({topic: topic, payload: "CHEER " + pattern});
-		}
-	});
+		stream.on('data', function (data) {
+			var pattern = data.text.toUpperCase().each(/\b(red|blue|green|purple)\b/i, function(color) {return color.first()}).join(""); 
+			if (pattern.length > 0) {
+				util.log(pattern + " - " + data.text);
+				mqtt_client.publish({topic: topic, payload: "CHEER " + pattern});
+			}
+		});
 	
 //  setInterval(function(){console.log(stream)}, 30000);
 
-	stream.on('end', function (response) {
-		// Handle a disconnection
-		console.log("Stream ended :" + response);
-	});
+		stream.on('end', function (response) {
+			// Handle a disconnection
+			util.log("Stream ended :" + response);
+			open_twitter_stream(mqtt_client);
+		});
 	
-	stream.on('destroy', function (response) {
-		// Handle a 'silent' disconnection from Twitter, no end/error event fired
-		console.log("Stream destroyed :" + response);
-	});
+		stream.on('destroy', function (response) {
+			// Handle a 'silent' disconnection from Twitter, no end/error event fired
+			util.log("Stream destroyed :" + response);
+		});
 	
-});
+	});
+};
 
-//setInterval(function(){console.log(twitter)}, 30000);
 
-new cronJob('00 00 17 * * *', function(){
-    client.publish({topic: topic, payload: "ON"});
-    console.log('Switch lights on');
-}, null, true);
-
-new cronJob('00 30 22 * * *', function(){
-    client.publish({topic: topic, payload: "OFF"});
-    console.log('Switch lights off');
-}, null, true);
+var mqtt_client = connect_to_mqtt(when_connected_to_mqtt);
 
